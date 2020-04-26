085069918bc62f4f1064da97d3554f9e4c49294f
pbollom <ptbollom@gmail.com>
2020-04-26 18:27:00 -0400

    second version of character counting

--- K&R/Ch1/characterCounting.c
+++ K&R/Ch1/characterCounting.c
@@ -6,12 +6,9 @@ Counts characters in input */
 
 int main()
 {
-    long nc;
+    double nc;
 
-    nc = 0;
-    while (getchar() != EOF)
-    {
-        ++nc;
-    }
-    printf("%ld\n", nc);
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
 }
