cf2936579b895ad91cdff9486fe51ab155dcb743
pbollom <ptbollom@gmail.com>
2020-04-26 18:30:27 -0400

    add line counting

---
+++ K&R/Ch1/lineCounting.c
@@ -0,0 +1,22 @@
+/* Author: Patrick Bollom
+Date: 2020-04-26
+Description:
+Counts newlines in input */  
+#include<stdio.h>
+
+int main()
+{
+    int c;
+    long nl;
+
+    nl = 0;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n')
+        {
+            ++nl;
+        }
+    }
+
+    printf("%ld\n", nl);
+}
