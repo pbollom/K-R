0b914b29834f51c9add3c96521d7eac26e9a6910
pbollom <ptbollom@gmail.com>
2020-04-26 18:52:48 -0400

    add K&R 1-9, blank consolidation

---
+++ K&R/Ch1/blankConsolidation.c
@@ -0,0 +1,26 @@
+/* Author: Patrick Bollom
+Date: 2020-04-26
+Description:
+K&R 1-9 Consolidates multiple blanks into a single blank */  
+#include<stdio.h>
+
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+        {
+            while ((c = getchar()) == ' ')
+            {
+                ;
+            }
+            printf("%c", ' ');
+        }
+        if (c != EOF) /* because there is a getchar outside of the main loop, we need to check this here */
+        {
+            printf("%c", c);
+        }
+    }
+}
