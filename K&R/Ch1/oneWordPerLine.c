d80262d443e655581c7ab65342c0c514ead6d5d5
pbollom <ptbollom@gmail.com>
2020-04-27 07:03:16 -0400

    add word counting and one word per line, K&R 1-12

---
+++ K&R/Ch1/oneWordPerLine.c
@@ -0,0 +1,41 @@
+/* Author: Patrick Bollom
+Date: 2020-04-27
+Description:
+K&R 1-12 write the input with one word per line */
+#include<stdio.h>
+
+#define IN  1  /* inside a word */
+#define OUT 0  /* outside a word */
+
+int main()
+{
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF)
+    {
+        nc++;
+        if (c == '\n')
+        {
+            nl++;
+        }
+        if (c == '\n' || c == '\t' || c == ' ')
+        {
+            if (state == IN) /* only add a new line if we were in a word */
+            {
+                printf("%c", '\n');
+            }
+            state = OUT;
+        }
+        else 
+        {
+            printf("%c", c);
+            if (state == OUT)
+            {
+                state = IN;
+                ++nw;
+            }
+        }
+    }
+}
---
+++ K&R/Ch1/wordCounting.c
@@ -0,0 +1,35 @@
+/* Author: Patrick Bollom
+Date: 2020-04-27
+Description:
+Counts lines, words, and characters in input */  
+#include<stdio.h>
+
+#define IN  1  /* inside a word */
+#define OUT 0  /* outside a word */
+
+int main()
+{
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF)
+    {
+        nc++;
+        if (c == '\n')
+        {
+            nl++;
+        }
+        if (c == '\n' || c == '\t' || c == ' ')
+        {
+            state = OUT;
+        }
+        else if (state == OUT)
+        {
+            state = IN;
+            ++nw;
+        }
+    }
+
+    printf("%d %d %d\n", nl, nw, nc);
+}
