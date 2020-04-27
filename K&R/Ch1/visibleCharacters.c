3e2b280967731e28e57f09fe4b31f55aff7941cf
pbollom <ptbollom@gmail.com>
2020-04-27 06:45:09 -0400

    add K&R 1-10, making tab, backspace, and slash non-ambiguous

---
+++ K&R/Ch1/visibleCharacters.c
@@ -0,0 +1,30 @@
+/* Author: Patrick Bollom
+Date: 2020-04-26
+Description:
+K&R 1-10 makes tab, backspace, and backslash visible */  
+#include<stdio.h>
+
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t')
+        {
+            printf("%c%c", '\\', 't');
+        }
+        else if (c == '\b')
+        {
+            printf("%c%c", '\\', 'b');
+        }
+        else if (c == '\\')
+        {
+            printf("%c%c", '\\', '\\');
+        }
+        else
+        {
+            printf("%c", c);
+        }   
+    }
+}
