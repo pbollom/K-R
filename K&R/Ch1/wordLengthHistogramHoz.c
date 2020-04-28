0b5f41a9815febb57bee1364e791937144046428
pbollom <ptbollom@gmail.com>
2020-04-28 06:48:24 -0400

    add gitignore

---
+++ K&R/Ch1/wordLengthHistogramHoz.c
@@ -0,0 +1,13 @@
+/* Author: pbollom
+Date: 2020-04-28
+Description:
+Prints a horizontal histogram of word lengths in an input */  
+#include<stdio.h>
+
+#define IN  1  /* inside a word */
+#define OUT 0  /* outside a word */
+
+int main()
+{
+
+}
