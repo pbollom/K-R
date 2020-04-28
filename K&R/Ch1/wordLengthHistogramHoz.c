4af6cdd1b95349f3bfbb7efba38addcdf1a37a46
pbollom <ptbollom@gmail.com>
2020-04-28 07:25:40 -0400

    create horizontal histogram of word lengths

--- K&R/Ch1/wordLengthHistogramHoz.c
+++ K&R/Ch1/wordLengthHistogramHoz.c
@@ -4,10 +4,65 @@ Description:
 Prints a horizontal histogram of word lengths in an input */  
 #include<stdio.h>
 
-#define IN  1  /* inside a word */
-#define OUT 0  /* outside a word */
+#define IN          1  /* inside a word */
+#define OUT         0  /* outside a word */
+#define MAXLENGTH   9 /* maximum discretely handled length */
 
 int main()
 {
+    int i, j, c, state, wordLength;
+    int nlength[MAXLENGTH + 1];
 
+    wordLength = 0;
+    for (i = 0; i < MAXLENGTH + 1; ++i)
+    {
+        nlength[i] = 0;
+    }
+
+    state = OUT;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n' || c == '\t' || c == ' ')
+        {
+            if (state == IN)
+            {
+                state = OUT;
+
+                if (wordLength > MAXLENGTH) /* We handle word lengths up to 9, after that it's a single bucket */
+                {
+                    ++nlength[MAXLENGTH];
+                }
+                else
+                {
+                    ++nlength[wordLength - 1];
+                }
+
+                wordLength = 0;
+            }
+        }
+        else
+        {
+            if (state == OUT)
+            {
+                state = IN;
+            }
+            ++wordLength;
+        }
+    }
+
+    for (i = 0; i < MAXLENGTH; ++i)
+    {
+        printf("%c  :", '1' + i);
+        for (j = 0; j < nlength[i]; ++j)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    printf("10+:");
+    for (j = 0; j < nlength[MAXLENGTH]; ++j)
+    {
+        printf("*");
+    }
+    printf("\n");
 }
