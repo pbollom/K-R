559e4f0846b4fc22404ac31db0b211056caa6157
pbollom <ptbollom@gmail.com>
2020-05-02 11:21:49 -0400

    add specialized version of longest line input - use extern

---
+++ Ch1/longestLineWithExtern.c
@@ -0,0 +1,72 @@
+/* Author: pbollom
+Date: 2020-05-02
+Description:
+Just like maxInputLineLength, but using external variables rather than arguments to the functions. */  
+#include<stdio.h>
+
+#define MAXLINE 1000    /* maximum input line size */
+
+int max;                /* maximum line length seen so far */
+char line[MAXLINE];     /* current input line */
+char longest[MAXLINE];  /* the lontest input line seen so far */
+
+int gtline(void);
+void copy(void);
+
+/* print the longest input line: specialized version */
+int main()
+{
+    int len;
+    extern int max;
+    extern char longest[];
+
+    while ((len = gtline()) > 0)
+    {
+        if (len > max)
+        {
+            max = len;
+            copy();
+        }
+    }
+    if (max > 0) /* there was a line */
+    {
+        printf("%s", longest);
+    }
+
+    return 0;
+}
+
+/* gtline: specialized version */
+int gtline(void)
+{
+    int c, i;
+    extern char line[];
+
+    for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    {
+        line[i] = c;
+    }
+    if (c == '\n')
+    {
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
+
+    return i;
+}
+
+/* copy: specialized version */
+void copy(void)
+{
+    int i;
+    extern char line[], longest[];
+
+    i = 0;
+    while ((longest[i] = line[i]) != '\0')
+    {
+        ++i;
+    }
+
+    return;
+}
