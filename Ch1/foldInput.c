7f03e93fb3c003cfdf4058e6078639a0277a48a5
pbollom <ptbollom@gmail.com>
2020-05-04 20:40:39 -0400

    fix issues with foldInput and simplify logic

--- Ch1/foldInput.c
+++ Ch1/foldInput.c
@@ -1,63 +1,48 @@
 /* Author: pbollom
 Date: 2020-05-04
 Description:
-1-22 Folds the input into strings of at most length FOLDLENGTH 
-Right now this is incorrect because it doesn't handle non-space blanks
-and it doesn't fold after the last non-blank character - it just folds on 
-the blank. Also, the input length is locked to one too small right now. */  
+1-22 Folds the input into strings of at most length FOLDLENGTH */  
 #include<stdio.h>
 
 #define FOLDLENGTH  10  /* the maximum linebreak spacing */
 
 int main()
 {
-    int c, i, lastspace, j;
-    char str[FOLDLENGTH + 1]; /* need one extra character at the end for null terminating */
+    int c, charindex, lastblank, i;
+    char str[FOLDLENGTH];
 
-    i = -1;
-    lastspace = -1;
+    charindex = -1;
+    lastblank = FOLDLENGTH - 1;
     while ((c = getchar()) != EOF)
     {
-        ++i;
-        str[i] = c;
+        ++charindex;
+        str[charindex] = c;
 
         if (c == '\n') /* special case or manually entered newline */
         {
-            str[i+1] = '\0';
+            str[charindex+1] = '\0';
             printf("%s", str);
-            lastspace = -1;
-            i = -1;
+            lastblank = FOLDLENGTH - 1;
+            charindex = -1;
         }
-        else 
+        else if (c == ' ' || c == '\t')
         {
-            if (c == ' ')
-            {
-                lastspace = i;
-            } 
+            lastblank = charindex;
         }
         
-        if (i == (FOLDLENGTH - 1)) /* hit the cap, so print out what we have */
+        if (charindex == (FOLDLENGTH - 1)) /* hit the cap, so print out what we have */
         {
-            if (lastspace == -1) /* special case, no spaces */
-            {
-                str[FOLDLENGTH] = '\0';
-                printf("%s\n", str);
-                i = -1;
-                lastspace = -1;
-            }
-            else /* replace the last space with a null */
-            {
-                str[lastspace] = '\0';
-                printf("%s\n", str);
+            /* this is where defaulting lastblank to FOLDLENGTH pays off - no conditional! */
+            str[lastblank] = '\0';
+            printf("%s\n", str);
                 
-                for (j = 0; j < FOLDLENGTH - lastspace; ++j)
-                {
-                    str[j] = str[lastspace + j + 1];
-                }
-
-                i = FOLDLENGTH - lastspace - 2;
-                lastspace = -1;
+            for (i = 0; i < FOLDLENGTH - lastblank; ++i)
+            {
+                str[i] = str[lastblank + i + 1];
             }
+
+            charindex = FOLDLENGTH - lastblank - 2; /* increment charindex first in the loop, need on extra offset */
+            lastblank = FOLDLENGTH - 1;
         }
     }
 }
