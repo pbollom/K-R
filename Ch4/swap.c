1298e9ef2f3e3721af012ffed15c2293ec93ed87
pbollom <ptbollom@gmail.com>
2020-05-31 20:16:19 -0400

    support multiple data types in the same block

--- Ch4/swap.c
+++ Ch4/swap.c
@@ -3,21 +3,21 @@ Date: 2020-05-31
 Description: 4-14 a macro to swap two arguments of type t */
 #include<stdio.h>
 
-#define swap(t, x, y) t temp = x; x = y; y = temp;
+#define swap(t, x, y) {t temp = x; x = y; y = temp;}
 
 int main()
 {
-    /*int x, y;
+    int x, y;
     x = 2;
     y = -2;
     swap(int, x, y);
-    printf("x=%d y=%d\n", x, y);*/
+    printf("x=%d y=%d\n", x, y);
 
-    char x, y;
-    x = 'p';
-    y = 'B';
-    swap(char, x, y);
-    printf("x=%c y=%c\n", x, y);
+    char x2, y2;
+    x2 = 'p';
+    y2 = 'B';
+    swap(char, x2, y2);
+    printf("x=%c y=%c\n", x2, y2);
 
     return 0;
 }
